79931d85c18966a60a565976df2b810180c17be6
IhorMeniv <ihormeniv@gmail.com>
2020-09-25 10:07:46 +0300

    forget create read from file

--- answer.py
+++ answer.py
@@ -1,15 +1,26 @@
+import time
+import random
+from libery import MicroLibrary
+
 # 1
-'''There is string s = "string". Write the code that reverses string, i.e. returns "gnirts"'''
+'''There is string s = "string".
+Write the code that reverses string,
+i.e. returns "gnirts"'''
 print("string"[::-1])
 
 # 2
 '''What is the purpose of decorators?'''
 
-#Decorator is a function that takes another function (decorated function) as an input and it can execute extra logic before/after trigering decorated function
+print(
+    '''Decorator is a function that takes another function (decorated function)
+    as an input and it can execute extra logic
+    before/after trigering decorated function'''
+)
 
 # 3
-'''Write decorator that measures function execution time and print it to output console.'''
-import time
+'''Write decorator that measures function execution
+time and print it to output console.'''
+
 
 def timer(func):
     def wrapper():
@@ -20,28 +31,32 @@ def timer(func):
 
     return wrapper
 
+
 @timer
 def sleep_function():
     time.sleep(3)
 
+
 print(sleep_function())
 
-
-#4
-'''Create micro library that allows users to work with notes about books authors.
-Note should contain author_name, note, rating (rating - is 0 - 1 rating of the author)
+# 4
+'''Create micro library that allows users
+to work with notes about books authors.
+Note should contain author_name, note,
+rating (rating - is 0 - 1 rating of the author)
 Micro lib should contain the next funcitonality'''
-import random
-from libery import MicroLibrary
 
 
 num = random.random()
 num = '%.2f' % num
 name_list = ['Andrii', 'German', 'Taras', 'Ihor', 'Oles', 'Ira']
+# used random for give the same users create the different note with different rating
 index = random.randint(0, 5)
 index_name = name_list[index]
 aut1 = MicroLibrary(f'{index_name}', f'{num}')
+
 aut1.add_note()
+aut1.read_from_file()
 aut1.dell_from_file()
 aut1.highest_rating()
 aut1.lower_rating()
--- libery.py
+++ libery.py
@@ -1,82 +1,93 @@
 class MicroLibrary:
 
-	dict_ = {}
-	author_rating = {}
+    dict_ = {}
+    author_rating = {}
 
-	def __init__(self, author, rating):
-		self.author = author
-		self.rating = rating
+    def __init__(self, author, rating):
+        self.author = author
+        self.rating = rating
 
-	def add_note(self):
-		self.note = input('write note for add: ')
-		self.dict_[self.author] = self.note
-		data = []
-		data.append(self.author)
-		data.append(self.note)
-		data.append(self.rating)
-		with open('note.txt', 'a+') as outfile:
-			for listitem in data:
-				outfile.write('%s,' % listitem)
-			outfile.write('\n')
-			print('Note append')
+    def add_note(self):
+        self.note = input('write note for add: ')
+        self.dict_[self.author] = self.note
+        data = []
+        data.append(self.author)
+        data.append(self.note)
+        data.append(self.rating)
+        with open('note.txt', 'a+') as outfile:
+            for listitem in data:
+                outfile.write('%s,' % listitem)
+            outfile.write('\n')
+            print('Note append')
 
-	def dell_from_file(self):
-		self.note = input('write note for del: ')
+    def read_from_file(self):
+        with open("note.txt", "r+") as f:
+            lines = f.readlines()
+            f.seek(0)
 
-		with open("note.txt", "r+") as f:
-			lines = f.readlines()
-			f.seek(0)
+            for line in lines:
+                text = line.split(',')
+                author = text[0]
+                note = text[1]
+                print(f'{author}'.upper() + ' write the note: ' + f'{note}')
 
-			for line in lines:
-				text = line.split(',')
-				if self.note == text[1]:
-					print('Note ' + self.note +' remove')
-					continue
-				else:
-					f.write(line)
-			f.truncate()
+    def dell_from_file(self):
+        self.note = input('write note for del: ')
 
-	def average_rating(self):
-		rating_a = 0
-		count = 0
+        with open("note.txt", "r+") as f:
+            lines = f.readlines()
+            f.seek(0)
 
-		with open("note.txt", "r+") as f:
-			lines = f.readlines()
-			f.seek(0)
+            for line in lines:
+                text = line.split(',')
+                if self.note == text[1]:
+                    print('Note ' + self.note + ' remove')
+                    continue
+                else:
+                    f.write(line)
+            f.truncate()
 
-			for line in lines:
-				rating = line.split(',')
-				count += 1
-				rating_a += float(rating[2])
-		average_rating = rating_a / count
-		print('average_rating: ' + '%.2f' % average_rating)	
+    def average_rating(self):
+        rating_a = 0
+        count = 0
 
-	def highest_rating(self):
-		highest_rating = 0
-		name_and_rating = None
+        with open("note.txt", "r+") as f:
+            lines = f.readlines()
+            f.seek(0)
 
-		with open("note.txt", "r+") as f:
-			lines = f.readlines()
-			f.seek(0)
+            for line in lines:
+                rating = line.split(',')
+                count += 1
+                rating_a += float(rating[2])
+        average_rating = rating_a / count
+        print('average_rating: ' + '%.2f' % average_rating) 
 
-			for line in lines:
-				rating = line.split(',')
-				if float(highest_rating) < float(rating[2]):
-					highest_rating = float(rating[2])
-					name_and_rating = rating[0] + ' ' + rating[2]
-			print("highest_rating: " + name_and_rating)
+    def highest_rating(self):
+        highest_rating = 0
+        name_and_rating = None
 
-	def lower_rating(self):
-		lower_rating = 1
-		name_and_rating = None
+        with open("note.txt", "r+") as f:
+            lines = f.readlines()
+            f.seek(0)
 
-		with open("note.txt", "r+") as f:
-			lines = f.readlines()
-			f.seek(0)
+            for line in lines:
+                rating = line.split(',')
+                if float(highest_rating) < float(rating[2]):
+                    highest_rating = float(rating[2])
+                    name_and_rating = rating[0] + ' ' + rating[2]
+            print("highest_rating: " + name_and_rating)
 
-			for line in lines:
-				rating = line.split(',')
-				if float(lower_rating) > float(rating[2]):
-					lower_rating = float(rating[2])
-					name_and_rating = rating[0] + ' ' + rating[2]
-			print("lower_rating: " + name_and_rating)
+    def lower_rating(self):
+        lower_rating = 1
+        name_and_rating = None
+
+        with open("note.txt", "r+") as f:
+            lines = f.readlines()
+            f.seek(0)
+
+            for line in lines:
+                rating = line.split(',')
+                if float(lower_rating) > float(rating[2]):
+                    lower_rating = float(rating[2])
+                    name_and_rating = rating[0] + ' ' + rating[2]
+            print("lower_rating: " + name_and_rating)
